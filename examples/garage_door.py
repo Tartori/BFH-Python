0c330cd498bf541b0c9574d5bf6654534a29045c
Julian <julianstampfli4@gmail.com>
2018-03-19 16:56:47 +0100

    garage door

--- examples/garage_door.py
+++ examples/garage_door.py
@@ -1,7 +1,45 @@
 """
 Implementation of garage door openering using dictionary of state transitions functions
 and action functions.
+
+>>> state_machine('closed', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.OFF
+'closed'
+>>> state_machine('closed', Input(open_button=True, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.UP
+'opening'
+>>> state_machine('closed', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.OFF
+'closed'
+>>> state_machine('opening', Input(open_button=False, close_button=False, limit_switch_open=True, limit_switch_closed=False))
+MotorAction.OFF
+'open'
+>>> state_machine('opening', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.DOWN
+'closing'
+>>> state_machine('opening', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.UP
+'opening'
+>>> state_machine('open', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.DOWN
+'closing'
+>>> state_machine('open', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.OFF
+'open'
+>>> state_machine('closing', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=True))
+MotorAction.OFF
+'closed'
+>>> state_machine('closing', Input(open_button=True, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.UP
+'opening'
+>>> state_machine('closing', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.DOWN
+'closing'
+>>> state_machine('nonexistent', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
+MotorAction.OFF
+'error'
 """
+
 from collections import namedtuple
 from enum import Enum, auto
 
@@ -25,61 +63,25 @@ state_transitions = {
                                 'closing' if input.close_button else 'opening'),
     'closing': lambda input: 'closed' if input.limit_switch_closed else (
                                 'opening' if input.open_button else 'closing'),
-    'error': lambda input: 'error'
+    'error': lambda input: 'errorbla'
 }
 
 # Action that are performed on every state on the outgoing edge.
 motor_action_transitions = {
     'closed': lambda input:  MotorAction.UP if input.open_button else MotorAction.OFF,
-    'open':  lambda input: None,        # YOUR CODE HERE
-    'opening': lambda input: None,      # YOUR CODE HERE
-    'closing': lambda input: None,      # YOUR CODE HERE
-    'error': lambda input: None         # YOUR CODE HERE
+    'open':  lambda input: MotorAction.DOWN if input.close_button else MotorAction.OFF,        # YOUR CODE HERE
+    'opening': lambda input: MotorAction.OFF if input.limit_switch_open else (
+        MotorAction.DOWN if input.close_button else MotorAction.UP),      # YOUR CODE HERE
+    'closing': lambda input: MotorAction.OFF if input.limit_switch_closed else (
+        MotorAction.UP if input.open_button else MotorAction.DOWN),      # YOUR CODE HERE
+    'error': lambda input: MotorAction.OFF         # YOUR CODE HERE
 }
 
 
 def state_machine(current_state, input):
-    """Determines action and next state.
-
-    >>> state_machine('closed', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.OFF
-    'closed'
-    >>> state_machine('closed', Input(open_button=True, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.UP
-    'opening'
-    >>> state_machine('closed', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.OFF
-    'closed'
-    >>> state_machine('opening', Input(open_button=False, close_button=False, limit_switch_open=True, limit_switch_closed=False))
-    MotorAction.OFF
-    'open'
-    >>> state_machine('opening', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.DOWN
-    'closing'
-    >>> state_machine('opening', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.UP
-    'opening'
-    >>> state_machine('open', Input(open_button=False, close_button=True, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.DOWN
-    'closing'
-    >>> state_machine('open', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.OFF
-    'open'
-    >>> state_machine('closing', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=True))
-    MotorAction.OFF
-    'closed'
-    >>> state_machine('closing', Input(open_button=True, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.UP
-    'opening'
-    >>> state_machine('closing', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.DOWN
-    'closing'
-    >>> state_machine('nonexistent', Input(open_button=False, close_button=False, limit_switch_open=False, limit_switch_closed=False))
-    MotorAction.OFF
-    'error'
-    """
+    """Determines action and next state."""
     next_state = state_transitions.get(current_state, lambda input: 'error')(input)
-    action = motor_action_transitions.get(current_state, lambda input: None)(input)
+    action = motor_action_transitions.get(current_state, lambda input: MotorAction.OFF)(input)
     print(action)
     return next_state
 
